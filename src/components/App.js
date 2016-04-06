dc27a57322a737250203a64dccfd0a18a315e722
Tanner Collin <git@tannercollin.com>
2016-04-05 20:30:28 -0600

    Make a menu bar for issue #2.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Menu from "./Menu/Menu.js";
 
 export default React.createClass({
 
     render: function () {
         return (
             <div>
+                <Menu/>
                 {this.props.children}
             </div>
         );
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { getCurrentUser, logout, checkAuth } from "../../lib/auth";
+import { Link, browserHistory } from "react-router";
+
+export default React.createClass({
+
+    onLogout: function () {
+        logout();
+        browserHistory.push("/signin");
+    },
+
+    render: function() {
+        return (
+            <div className="ui stackable inverted menu">
+              <div className="ui container">
+                <a href="#" className="header item">
+                  <img className="logo" src="http://i.imgur.com/pXlkRaH.png"/> { " Renti" }
+                </a>
+                <Link to={"/"} className="item">Home</Link>
+                <div className="right menu">
+                    { checkAuth() ? <Link to="/user/admin" className="item">{ getCurrentUser().first_name }</Link> : <Link to="/signup" className="item">Sign Up</Link> }
+                    { checkAuth() ? <div className="item"><button onClick={this.onLogout} className="ui red button">Log out</button></div> : <div className="item"><Link to="/signin" className="ui blue button">Log In</Link></div> }
+                </div>
+              </div>
+            </div>
+        );
+    }
+});
