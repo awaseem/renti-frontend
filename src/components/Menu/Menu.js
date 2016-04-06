729bfdd6eecabaa4f2fa1bb9bea086b60bab4117
Tanner Collin <git@tannercollin.com>
2016-04-05 21:02:09 -0600

    Move menu elements to the left to fix stacking issue.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -17,10 +17,8 @@ export default React.createClass({
                   <img className="logo" src="http://i.imgur.com/pXlkRaH.png"/> { " Renti" }
                 </a>
                 <Link to={"/"} className="item">Home</Link>
-                <div className="right menu">
                     { checkAuth() ? <Link to="/user/admin" className="item">{ getCurrentUser().first_name }</Link> : <Link to="/signup" className="item">Sign Up</Link> }
                     { checkAuth() ? <div className="item"><button onClick={this.onLogout} className="ui red button">Log out</button></div> : <div className="item"><Link to="/signin" className="ui blue button">Log In</Link></div> }
-                </div>
               </div>
             </div>
         );
