5ecab19c97af567c73ab2d452631011d424d01f3
Tanner Collin <git@tannercollin.com>
2016-04-08 13:17:46 -0600

    Remove redundant rows/columsn from Signin and Signup. Pad header.

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -75,67 +75,60 @@ export default React.createClass({
 
     render: function () {
         return (
-            <div className="ui centered grid">
-                <div className="row">
-                    <div className="column">
-                        <h1 className="ui center aligned header">Renti</h1>
-                        <h3 className="ui center aligned header"><i>Rent, Share, Ride</i></h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="column">
-                        <div className="ui segment">
-                            <form id="sign-in-form" className="ui form">
-                                <div className="field">
-                                    <label>Username</label>
-                                    <input type="text" ref="username" name="username" placeholder="Enter a username"></input>
-                                </div>
-                                <div className="field">
-                                    <label>Password</label>
-                                    <input type="password" ref="password" name="password" placeholder="Enter a Password"></input>
-                                </div>
-                                <div className="field">
-                                    <label>Image</label>
-                                    <input type="text" ref="image" name="image" placeholder="Enter an image URL"></input>
-                                </div>
-                                <div className="equal width fields">
-                                    <div className="field">
-                                        <label>First Name</label>
-                                        <input type="text" ref="first_name" name="first_name" placeholder="Enter your first name"></input>
-                                    </div>
-                                    <div className="field">
-                                        <label>Last Name</label>
-                                        <input type="text" ref="last_name" name="last_name" placeholder="Enter your last name"></input>
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label>Date of Birth</label>
-                                    <DatePicker
-                                        selected={this.state.dateOfBirth}
-                                        showYearDropdown
-                                        onChange={this.handleChange} />
-                                </div>
-                                <div className="equal width fields">
-                                    <div className="field">
-                                        <label>Address</label>
-                                        <input type="text" ref="address" name="address" placeholder="Enter an address"></input>
-                                    </div>
-                                    <div className="field">
-                                        <label>Email Address</label>
-                                        <input type="text" ref="emailAddress" name="emailAddress" placeholder="Enter your email"></input>
-                                    </div>
-                                </div>
-                                <div className="field">
-                                    <label>Summary</label>
-                                    <textarea rows="2" ref="summary" name="summary" placeholder="Enter a summary about yourself"></textarea>
-                                </div>
-                                <button className="ui green button">Submit</button>
-                                <div style={{ display: this.state.error ? "block" : "none"}} className="ui error message">{this.state.error}</div>
-                            </form>
+        <div className="ui centered padded grid">
+            <div className="column">
+                <h1 className="ui center aligned header">Sign up for Renti</h1>
+                <div className="ui segment">
+                    <form id="sign-in-form" className="ui form">
+                        <div className="field">
+                            <label>Username</label>
+                            <input type="text" ref="username" name="username" placeholder="Enter a username"></input>
                         </div>
-                    </div>
+                        <div className="field">
+                            <label>Password</label>
+                            <input type="password" ref="password" name="password" placeholder="Enter a Password"></input>
+                        </div>
+                        <div className="field">
+                            <label>Image</label>
+                            <input type="text" ref="image" name="image" placeholder="Enter an image URL"></input>
+                        </div>
+                        <div className="equal width fields">
+                            <div className="field">
+                                <label>First Name</label>
+                                <input type="text" ref="first_name" name="first_name" placeholder="Enter your first name"></input>
+                            </div>
+                            <div className="field">
+                                <label>Last Name</label>
+                                <input type="text" ref="last_name" name="last_name" placeholder="Enter your last name"></input>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label>Date of Birth</label>
+                            <DatePicker
+                             selected={this.state.dateOfBirth}
+                             showYearDropdown
+                             onChange={this.handleChange} />
+                        </div>
+                        <div className="equal width fields">
+                            <div className="field">
+                                <label>Address</label>
+                                <input type="text" ref="address" name="address" placeholder="Enter an address"></input>
+                            </div>
+                            <div className="field">
+                                <label>Email Address</label>
+                                <input type="text" ref="emailAddress" name="emailAddress" placeholder="Enter your email"></input>
+                            </div>
+                        </div>
+                        <div className="field">
+                            <label>Summary</label>
+                            <textarea rows="2" ref="summary" name="summary" placeholder="Enter a summary about yourself"></textarea>
+                        </div>
+                        <button className="ui green button">Submit</button>
+                        <div style={{ display: this.state.error ? "block" : "none"}} className="ui error message">{this.state.error}</div>
+                    </form>
                 </div>
             </div>
+        </div>
         );
     }
 });
